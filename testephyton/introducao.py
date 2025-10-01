247f83dedbcb0ea7adb5500e310c1873ca2cfd8e
0Vidal <oliveira.vidal.vinicius@escola.pr.gov.br>
2025-10-01 17:38:33 -0300

    Add files via upload

---
+++ testephyton/introducao.py
@@ -0,0 +1,16 @@
+idade =  1
+
+if idade > 18:
+    print("Você é maior de idade")
+    print("bloco 1")  
+else:
+     print("Você não é maior de idade")
+     print("bloco 2")
+
+for i in range(0,11):
+     print(i)
+
+contador = 1
+while contador <=  10:
+     print(contador) 
+     contador +=1    
